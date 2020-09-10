1002e5b3e0ed621c63cd2c3697045ab2b5882651
rhomere <rhomere@gmail.com>
2020-09-10 09:11:20 -0400

    added Views folders for Razr and AngularJs, added AngularJs core nuget

--- Template/Services.Api.Mvc.Core/Controllers/HelloWorldController.cs
+++ Template/Services.Api.Mvc.Core/Controllers/HelloWorldController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -16,14 +17,17 @@ namespace Services.Api.Mvc.Core.Controllers
         public string Index()
         {
             return "This is my default action...";
+            //return View();
         }
 
         // 
         // GET: /HelloWorld/Welcome/ 
 
-        public string Welcome()
+        public string Welcome(string name, int id = 1)
         {
-            return "This is the Welcome action method...";
+            //return "This is the Welcome action method...";
+
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {id}");
         }
     }
 }
